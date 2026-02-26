01edee47bf15a5367caa6549e202fa6a49639835
wenbo5867-cyber <wenbo5867@gmail.com>
2026-02-26 09:47:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def download(task_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
